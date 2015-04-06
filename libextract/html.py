833fda0c59daf46e1710e0341f654c889fcbed2a
Eugene Eeo <packwolf58@gmail.com>
2015-04-06 21:23:59 +0800

    document html module

--- libextract/html.py
+++ libextract/html.py
@@ -17,21 +17,37 @@ FILTER_TEXT = './/*[not(self::script or self::style or \
 
 
 def node_text_length(node):
+    """
+    Returns the length of the text contained within
+    a given *node*.
+    """
     return len(''.join(node.xpath(TEXT_IN_NODE)))
 
 
 def get_etree(document):
+    """
+    Get an ElementTree instance from a given XML/HTML
+    *document*. The encoding is automatically detected.
+    """
     return parse(BytesIO(document),
                  HTMLParser(encoding=detect(document)['encoding'],
                             remove_blank_text=True))
 
 
 def get_pairs(etree):
+    """
+    Given an *etree*, returns an iterable of parent
+    to node text length pairs.
+    """
     for node in etree.xpath(NODES_WITH_TEXT):
         yield node.getparent(), node_text_length(node)
 
 
 def get_final_text(node):
+    """
+    Gets the text contained within the children node
+    of a given *node*, joined by a space.
+    """
     return ' '.join(node.xpath(FILTER_TEXT))
 
 
