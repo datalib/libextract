7c2e3dfbdc31c1490d32454c0ae101cfddba3c0c
im-rodrigo <rodrigopala91@gmail.com>
2015-04-06 21:56:36 -0700

    made test-breaking change to one of our asset files; algo is currently broken :(

--- tests/test_libextract.py
+++ tests/test_libextract.py
@@ -2,6 +2,9 @@ import os
 from unittest import TestCase
 from tests import asset_path
 from libextract import extract
+from libextract.html import get_etree
+
+
 
 FOOS_FILENAME = asset_path('full_of_foos.html')
 
@@ -17,6 +20,10 @@ class TestLibExtract(TestCase):
     def test_is_str(self):
         self.assertTrue(isinstance(self.content, str))
 
-    def test_str_is_foos(self, ):
+    def test_str_is_foos(self):
         foos = "foo. foo. foo. foo. foo. foo. foo. foo. foo."
         self.assertEqual(self.content, foos)
+
+    def test_get_etree_is_correct_article_etree(self):
+        pass
+
