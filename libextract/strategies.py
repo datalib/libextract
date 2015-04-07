c134cdbc31f12b5fe2b5ce72792c63bbfd496340
Eugene Eeo <packwolf58@gmail.com>
2015-04-07 14:37:23 +0800

    keep strategies DRY

--- libextract/strategies.py
+++ libextract/strategies.py
@@ -3,9 +3,11 @@ from .coretools import histogram, argmax
 from .html import get_etree, get_pairs, get_final_text
 
 
-ARTICLE_TEXT = (get_etree, get_pairs, histogram, argmax, get_final_text)
+_BASE = (get_etree, get_pairs, histogram, argmax)
 
-ARTICLE_NODE = (get_etree, get_pairs, histogram, argmax, lambda (n, _): n)
+ARTICLE_TEXT = _BASE + (get_final_text,)
+ARTICLE_NODE = _BASE + (lambda (n, _): n,)
+
 
 #shortcuts
 AT = ARTICLE_TEXT
