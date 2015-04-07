d87ed5775dfb04d60f639c94996c3750a08fbd2c
Eugene Eeo <packwolf58@gmail.com>
2015-04-07 08:39:47 +0800

    make tests run faster

--- libextract/tests/test_libextract.py
+++ libextract/tests/test_libextract.py
@@ -3,23 +3,20 @@ from unittest import TestCase
 from libextract import extract
 
 TESTS_DIR = os.path.dirname(__file__)
-
 FOOS_FILENAME = os.path.join(TESTS_DIR, 'assets/full_of_foos.html')
 
 
 class TestLibExtract(TestCase):
     def setUp(self):
         self.file = open(FOOS_FILENAME, 'r')
-        self.text = self.file.read()
+        self.content = extract(self.file.read())
 
     def tearDown(self):
         self.file.close()
 
     def test_is_str(self):
-        content = extract(self.text)
-        self.assertTrue(isinstance(content, str))
+        self.assertTrue(isinstance(self.content, str))
 
     def test_str_is_foos(self, ):
-        content = extract(self.text)
         foos = "foo. foo. foo. foo. foo. foo. foo. foo. foo."
-        self.assertEqual(content, foos)
+        self.assertEqual(self.content, foos)
