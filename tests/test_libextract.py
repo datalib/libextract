07a14dd0682e06bb51068af50d4434b61ae2e719
Eugene Eeo <packwolf58@gmail.com>
2015-04-08 17:08:55 +0800

    refactor tests

--- tests/test_libextract.py
+++ tests/test_libextract.py
@@ -5,25 +5,17 @@ from libextract import extract
 from libextract.html import get_etree
 
 
-
 FOOS_FILENAME = asset_path('full_of_foos.html')
 
 
 class TestLibExtract(TestCase):
     def setUp(self):
-        self.file = open(FOOS_FILENAME, 'r')
-        self.content = extract(self.file.read())
-
-    def tearDown(self):
-        self.file.close()
+        with open(FOOS_FILENAME, 'r') as fp:
+            self.content = extract(fp.read())
 
     def test_is_str(self):
-        self.assertTrue(isinstance(self.content, str))
+        assert isinstance(self.content, str)
 
     def test_str_is_foos(self):
         foos = "foo. foo. foo. foo. foo. foo. foo. foo. foo."
-        self.assertEqual(self.content, foos)
-
-    def test_get_etree_is_correct_article_etree(self):
-        pass
-
+        assert self.content == foos
