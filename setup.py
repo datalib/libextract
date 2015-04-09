e5538164e832f31efbb94c35632400a066fd70f0
Eugene Eeo <packwolf58@gmail.com>
2015-04-09 21:50:06 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,26 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='libextract',
-      version='0.0.0',
-      description='A ridiculously simple HT/XML article-text extractor.',
-      keywords='extract extraction main article text html data-extraction data\
-                content-extraction content unsupervised classification',
-      author='Rodrigo Palacios, ',
-      author_email='rodrigopala91@gmail.com, ',
-      license='MIT',
-      packages=['libextract', 'libextract.html'],
-      url='https://github.com/libextract/libextract',
-      scripts=[],
-      package_data={},
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name='libextract',
+    version='0.0.0',
+    url='https://github.com/libextract/libextract',
+    license='MIT',
+    description='A ridiculously simple HT/XML article-text extractor',
+    packages=['libextract', 'libextract.html'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'chardet>=2.3.0',
+        'lxml>=3.4.2',
+    ],
+    keywords='extract extraction main article text html data-extraction data\
+              content-extraction content unsupervised classification',
+    author='Rodrigo Palacios, ',
+    author_email='rodrigopala91@gmail.com, ',
+    scripts=[],
+    package_data={},
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
