be2451a76c66bfb06884a37467004f8c1066b244
Eugene Eeo <packwolf58@gmail.com>
2015-04-09 18:43:33 +0800

    node_json splits classes and ids

--- libextract/formatters.py
+++ libextract/formatters.py
@@ -1,10 +1,10 @@
 def node_json(node, depth=0):
     json = {
         'xpath': node.getroottree().getpath(node),
-        'class': node.get('class'),
+        'class': node.get('class', '').split(),
         'text': node.text,
         'tag': node.tag,
-        'id': node.get('id'),
+        'id': node.get('id', '').split(),
     }
     if depth:
         json['children'] = [node_json(n, depth-1) for n in node]
