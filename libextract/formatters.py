77bbdf63ad219d20142c2a96cc54ad42ba6c6350
Eugene Eeo <packwolf58@gmail.com>
2015-04-10 21:30:39 +0800

    document formatters [skip ci]

--- libextract/formatters.py
+++ libextract/formatters.py
@@ -1,17 +1,31 @@
+from functools import partial
+
+
 UNLIMITED = float('NaN')
 
 
 def split_node_attr(node, attr):
+    """
+    Given a *node*, split the *attr* of the node
+    into a list of strings, suitable for id/class
+    handling.
+    """
     return (node.get(attr) or '').split()
 
 
+get_node_id = partial(split_node_attr, attr='id')
+get_node_class = partial(split_node_attr, attr='class')
+
+
 def node_json(node, depth=0):
+    """
+    """
     return {
         'xpath': node.getroottree().getpath(node),
-        'class': split_node_attr(node, 'class'),
+        'class': get_node_class(node),
         'text': node.text,
         'tag': node.tag,
-        'id': split_node_attr(node, 'id'),
+        'id': get_node_id(node),
         'children': (
             [node_json(n, depth-1) for n in node] if depth else None
         ),
