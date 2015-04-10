e3322776cd98c94c8c8e0091f0b68d7598a38a8e
Eugene Eeo <packwolf58@gmail.com>
2015-04-10 18:29:49 +0800

    sometimes HtmlElement.get can return None

--- libextract/formatters.py
+++ libextract/formatters.py
@@ -4,10 +4,10 @@ UNLIMITED = float('NaN')
 def node_json(node, depth=0):
     return {
         'xpath': node.getroottree().getpath(node),
-        'class': node.get('class', '').split(),
+        'class': (node.get('class') or '').split(),
         'text': node.text,
         'tag': node.tag,
-        'id': node.get('id', '').split(),
+        'id': (node.get('id') or '').split(),
         'children': (
             [node_json(n, depth-1) for n in node] if depth else None
         ),
