ecbf98df7c6b28f37b64327e27b13fdc3c71d1be
Eugene Eeo <packwolf58@gmail.com>
2015-04-12 16:29:03 +0800

    test encoding argument

--- tests/test_libextract.py
+++ tests/test_libextract.py
@@ -2,9 +2,16 @@ from pytest import fixture
 from libextract import extract
 
 
+FOOS = "foo. foo. foo. foo. foo. foo. foo. foo. foo."
+
+
 def test_extract(foo_file):
     content = extract(foo_file.read())
-    foos = "foo. foo. foo. foo. foo. foo. foo. foo. foo."
 
     assert isinstance(content, str)
-    assert content == foos
+    assert content == FOOS
+
+
+def test_extract_encoding(foo_file):
+    content = extract(foo_file.read(), encoding='ascii')
+    assert content == FOOS
