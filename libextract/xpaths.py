81ab2e1486b66fd1e1ce961395e875d50a86a5fd
im-rodrigo <rodrigopala91@gmail.com>
2015-04-12 02:30:38 -0700

    moved xpaths to xpaths.py

---
+++ libextract/xpaths.py
@@ -0,0 +1,9 @@
+"""A handful of useful xpath expressions to help you along the way"""
+
+
+NODES_WITH_TEXT = '//*[not(self::script or self::style)]/\
+                     text()[normalize-space()]/..'
+
+FILTER_TEXT = './/*[not(self::script or self::style or \
+        self::figure or self::span or self::time)]/\
+        text()[normalize-space()]'
