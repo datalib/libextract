dcaf40a30ddcffd7508f8d06b5c2a9f57826f034
im-rodrigo <rodrigopala91@gmail.com>
2015-04-12 15:36:08 -0700

    added test cases for table formatter

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -1,7 +1,8 @@
 from pytest import fixture
 from copy import deepcopy
 from lxml import etree
-from libextract.formatters import node_json
+from lxml import html
+from libextract.formatters import node_json, get_table_as_header_rows_list
 
 
 @fixture
@@ -10,6 +11,28 @@ def elem():
         '<html class="this those" id="that"><p>Hello World</p></html>'
     )
 
+@fixture
+def table():
+    return html.fromstring(
+        '<table>\
+            <thead>\
+                <tr>\
+                    <th>Name</th>\
+                    <th>Gender</th>\
+                </tr>\
+            </thead>\
+            <tbody>\
+                <tr>\
+                    <td>Rodrigo</td>\
+                    <td>male</td>\
+                </tr>\
+                <tr>\
+                    <td>Eugene</td>\
+                    <td>male</td>\
+                </tr>\
+            </tbody>\
+        </table>'
+    )
 
 @fixture
 def json():
@@ -39,3 +62,10 @@ def test_depth(elem, json):
 
     assert node_json(elem, depth=1) == json
     assert node_json(elem, depth=2) == expected
+
+
+def test_get_table_as_header_rows_list(table):
+    lists = get_table_as_header_rows_list(table)
+    assert lists[0] == ['Name', 'Gender']
+    assert lists[1] == ['Rodrigo', 'male']
+    assert lists[2] == ['Eugene', 'male']
