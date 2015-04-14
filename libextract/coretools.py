725fc9d97db4a7cf801bcef1baf473e5d8e3ef1c
im-rodrigo <rodrigopala91@gmail.com>
2015-04-13 23:27:56 -0700

    removed debug decorator from coretools.py

--- libextract/coretools.py
+++ libextract/coretools.py
@@ -54,61 +54,6 @@ def prunes(selector):
     return decorator
 
 
-def debug(fn):
-    """
-    This decorator will hook into a function's out
-    and print out the output value.
-
-    @debug
-    @node_processor
-    def if_table(node):
-        if node.tag == "table":
-            table = table_json(node)
-            for e, row in enumerate(table['Average female height']):
-                s = re.split("^(.*)cm", row)
-                table['Average female height'][e] = "".join(s[:2]).strip()
-            return table
-        else:
-            return node
-
-    Sample output:
-    
-    decorator running ... output: <generator object filter_tags...>)
-    Press <Enter> to continue
-    decorator running ... output:
-    {
-        'Sample population / age range': [
-                u'20\u201329',
-                u'19\u201349',
-                '17 (healthy)',
-                ...]
-    }
-    Press <Enter> to continue
-    decorator running ... output: <Element ol at 0x6111778>)
-    Press <Enter> to continue
-    decorator running ... output: <Element span at 0x61036d8>)
-    Press <Enter> to continue
-    decorator running ... output: <Element div at 0x38a4bd8>)
-    Press <Enter> to continue
-    decorator running ... output: <Element ul at 0x3af64a8>)
-    Press <Enter> to continue
-    """
-    @wraps(fn)
-    def decorator(*args):
-        print("{0} running ... output: {1})".format(fn.__name__, args))
-
-        input("Press <Enter> to continue")
-
-        data = fn(*args)
-        for d in data:
-            print("{0} running ... output: {1})".format(fn.__name__, d))
-            input("Press <Enter> to continue")
-
-        yield data
-
-    return decorator
-
-
 def node_processor(fn):
     """
     Use this decorator if you would like to create your own
