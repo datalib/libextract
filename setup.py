4ec813a92e1998b0e435e0944e9e50c07f2cc058
Eugene Eeo <packwolf58@gmail.com>
2015-04-15 22:12:43 +0800

    fixes #28

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
@@ -32,7 +32,7 @@ setup(
     url='https://github.com/libextract/libextract',
     license='MIT',
     description='A ridiculously simple HT/XML article-text extractor',
-    packages=['libextract'],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     platforms='any',
