03a755c1c2877979a833c0e43ba95360c15e5db0
Eugene Eeo <packwolf58@gmail.com>
2015-05-16 00:17:11 +0800

    put doctsrings in the right place

--- libextract/__init__.py
+++ libextract/__init__.py
@@ -1,1 +1,10 @@
+"""
+    libextract
+    ~~~~~~~~~~
 
+    Beautfully simple data extraction using simple,
+    composable pipelined functions.
+
+    :copyright: (c) 2015 Libextract
+    :license: MIT, see LICENSE for details.
+"""
