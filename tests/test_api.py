aa0cedc327e0e2ecb3800bfa532334076a95c88b
im-rodrigo <rodrigopala91@gmail.com>
2015-05-22 01:10:21 -0700

    added tests to test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
-from .fixtures import foo_file
-from libextract.api import extract, ARTICLE_TABLES
-
+from .fixtures import foo_file, etree
+from libextract.api import extract, pipeline, select, measure, rank, finalise
+from statscounter import StatsCounter
 
 def test_extract(foo_file):
     r = extract(foo_file)
@@ -12,12 +12,41 @@ def test_extract(foo_file):
 
 
 def test_extract_tabular(foo_file):
-    r = list(extract(foo_file, strategy=ARTICLE_TABLES))
+    r = list(extract(foo_file))
     u = [node.tag for node in r]
     assert u == [
         'article',
-        'html',
         'body',
     ]
     for node in r[0]:
         assert node.tag == 'div'
+
+
+def test_pipeline(etree):
+    funcs = (select, measure, rank, finalise)
+    assert list(pipeline(etree, funcs)) == [etree.xpath('//article')[0],
+                                            etree.xpath('//body')[0]]
+
+
+def test_select(etree):
+    assert select(etree) == etree.xpath('//body//*/..')
+
+
+def test_measure(etree):
+    rv = measure(etree.xpath('//div/..'))
+    uv = [(node.tag, metric) for node, metric in rv]
+
+    assert uv == [('article', StatsCounter(['div']*9))] 
+
+
+def test_rank(etree):
+    k = measure(etree.xpath('//body//*/..'))
+    rv = rank(k)
+
+    assert list(rv)[0] ==  (etree.xpath('//article')[0],
+                            StatsCounter(['div']*9))
+
+
+def test_finalise(etree):
+    datum = [('k', 1), ('b', 2)]
+    assert list(finalise(datum)) == ['k','b']
