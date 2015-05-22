d8cf72fd1d46cddd60e90c59ee5a2da69e435fde
im-rodrigo <rodrigopala91@gmail.com>
2015-05-22 01:10:34 -0700

    enabled verbose test output

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ class PyTest(TestCommand):
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
-        self.pytest_args = []
+        self.pytest_args = ['-v']
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
